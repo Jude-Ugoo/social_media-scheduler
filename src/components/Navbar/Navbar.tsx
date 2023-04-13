be0a90f244a25fa7b8662c75a528b3b3ac04204c
Jude-Ugoo <ugooeze0@gmail.com>
2023-04-13 21:06:47 +0100

    added case to hide navbar on login and signup pages

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,17 @@
 import { Flex, Heading } from "@chakra-ui/react";
 import React from "react";
+import { useLocation } from "react-router-dom";
 import NavOptions from "./NavbarOptions/NavOptions";
 import NavRight from "./NavbarRight/NavRight";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
+  const withoutNav = ["/signin", "/signup"];
+  const { pathname } = useLocation();
+
+  if (withoutNav.some((item) => pathname.includes(item))) return null;
+
   return (
     <Flex
       height="100px"
